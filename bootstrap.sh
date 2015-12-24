4efec44293ba6f8342dfbccc1fc5dfecf9400d55
Larry Dennison <ldennison@nvidia.com>
2015-12-24 09:35:56 -0800

    Initial

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,6 @@
 
 dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install -y http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install akmod-nvidia "kernel-devel-uname-r == $(uname -r)"
+dnf update -y
