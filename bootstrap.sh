b0921a1401e19124e8c02faabe6669753baf308c
Larry Dennison <ldennison@nvidia.com>
2015-12-24 09:32:42 -0800

    Initial

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
